de1e3b1851c7761ae3386faedbb2c5e78c59c012
floris <46795700+Floyo66@users.noreply.github.com>
2025-01-16 01:14:23 +0100

    adding test

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from functools import wraps  # For the login_required
+import os
+print("Current working directory:", os.getcwd())
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'your_secret_key'  # Required for session handling
